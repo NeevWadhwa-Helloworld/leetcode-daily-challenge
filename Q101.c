a08c048d0ee1e51694b6888647b89e5faff05ac0
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-04-13 21:13:38 +0530

    Implement functions to check binary tree symmetry

---
+++ Q101.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ * int val;
+ * struct TreeNode *left;
+ * struct TreeNode *right;
+ * };
+ */
+bool isMirror(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+    if (p == NULL || q == NULL || p->val != q->val) {
+        return false;
+    }
+    return isMirror(p->left, q->right) && isMirror(p->right, q->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL) {
+        return true;
+    }
+    return isMirror(root->left, root->right);
+}
