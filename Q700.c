18ca5554d6df95b7eea607b7fa314655de9cd8f6
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-05-03 11:21:27 +0530

    Implement search function for binary search tree

---
+++ Q700.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    while(root!=NULL){
+        if(root->val==val){
+            return root;
+        }else if(root->val>val){
+            root=root->left;
+        }else if(root->val<val){
+            root=root->right;
+        }
+    }
+    return NULL;
+}
