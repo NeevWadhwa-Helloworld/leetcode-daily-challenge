65560556e915ff3100b674b806b5f7943f854948
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-05-12 08:49:53 +0530

    Add minDepth function for binary tree
    Implement function to calculate minimum depth of a binary tree.

---
+++ Q111.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int minDepth(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    if (root->left == NULL) {
+        return 1 + minDepth(root->right);
+    }
+    if (root->right == NULL) {
+        return 1 + minDepth(root->left);
+    }
+    int leftMin = minDepth(root->left);
+    int rightMin = minDepth(root->right);
+
+    return 1 + (leftMin < rightMin ? leftMin : rightMin);
+}
