2590e3160dd3afc479ffa2697aa4cf35f379f696
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-06-26 10:28:00 +0530

    Create Q645.c

---
+++ Q645.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 2;
+    
+    int duplicate = -1;
+    int missing = -1;
+    for (int i = 0; i < numsSize; i++) {
+        int val = abs(nums[i]);
+        if (nums[val - 1] < 0) {
+            duplicate = val;
+        } else {
+            nums[val - 1] = -nums[val - 1];
+        }
+    }
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > 0) {
+            missing = i + 1;
+            break;
+        }
+    }
+    
+    result[0] = duplicate;
+    result[1] = missing;
+    
+    return result;
+}
