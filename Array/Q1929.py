04a2afb9d0b33fabe84d2cd435a376fbafc1d34e
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-09-05 11:29:03 +0530

    Implement getConcatenation method in Solution class

---
+++ Array/Q1929.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        final_nums=nums*2
+        return final_nums
