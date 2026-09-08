d1c80054021b75e0f4828cbd8b45d9f9d98b8431
Neev Wadhwa <neevwadhwa9568@gmail.com>
2026-09-08 07:50:38 +0530

    Implement countCommas function in Solution class

---
+++ Math/Q3870.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        long long totalCommas = 0;
+        long long threshold = 1000;
+        
+        while (n >= threshold) {
+            totalCommas += (n - threshold + 1);
+            threshold *= 1000;
+        }
+        
+        return totalCommas;
+    }
+};
